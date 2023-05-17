42a7fb399f1ca306d91e0ef70f914b22e7bc0078
PotatoAnimal <potatoanimals@gmail.com>
2023-05-17 22:09:57 +0300

    enum started

--- src/com/rmnk/keywords/enumKeyWord/WorkingWithEnumKeyWord.java
+++ src/com/rmnk/keywords/enumKeyWord/WorkingWithEnumKeyWord.java
@@ -2,7 +2,6 @@ package com.rmnk.keywords.enumKeyWord;
 
 public class WorkingWithEnumKeyWord {
     public static void main(String[] args) {
-        Colors red = Colors.RED;
-        System.out.println(red);
+        System.out.println("ye");
     }
 }
