5d1d2ee16548b23bddb1f3a3d114bd289fbffe16
PotatoAnimal <potatoanimals@gmail.com>
2023-05-17 22:44:05 +0300

    hehe

--- src/com/rmnk/keywords/enumKeyWord/WorkingWithEnumKeyWord.java
+++ src/com/rmnk/keywords/enumKeyWord/WorkingWithEnumKeyWord.java
@@ -2,29 +2,6 @@ package com.rmnk.keywords.enumKeyWord;
 
 public class WorkingWithEnumKeyWord {
     public static void main(String[] args) {
-        Colors day = Colors.BLACK;
-            Test test = new Test();
-            test.dayIsLike(day);
-        }
-        record Test (){
-            public void dayIsLike (Colors day) {
-                switch (day) {
-                    case BLACK:
-                        System.out.println("Mondays are bad.");
-                        break;
-                    case WHITE:
-                        System.out.println("Fridays are better.");
-                        break;
-                    case RED:
-                        System.out.println("ye");
-                        break;
-                    case YELLOW:
-                        System.out.println("Weekends are best.");
-                        break;
-                    default:
-                        System.out.println("Midweek days are so-so.");
-                        break;
-                }
-            }
-        }
+        System.out.println("everything");
+    }
 }
