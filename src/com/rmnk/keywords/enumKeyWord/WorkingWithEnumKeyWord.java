362f779c60fdb825e681f54f48298cc95585f993
PotatoAnimal <potatoanimals@gmail.com>
2023-05-17 22:10:24 +0300

    enum started

--- src/com/rmnk/keywords/enumKeyWord/WorkingWithEnumKeyWord.java
+++ src/com/rmnk/keywords/enumKeyWord/WorkingWithEnumKeyWord.java
@@ -3,5 +3,6 @@ package com.rmnk.keywords.enumKeyWord;
 public class WorkingWithEnumKeyWord {
     public static void main(String[] args) {
         System.out.println("ye");
+        System.out.println("hah");
     }
 }
