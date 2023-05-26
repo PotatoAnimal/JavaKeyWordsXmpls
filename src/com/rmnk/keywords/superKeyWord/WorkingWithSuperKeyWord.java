8a1897a9685aa025ec3a031efb15602f7556c866
PotatoAnimal <potatoanimals@gmail.com>
2023-05-26 17:49:28 +0300

    superkeyword not done

---
+++ src/com/rmnk/keywords/superKeyWord/WorkingWithSuperKeyWord.java
@@ -0,0 +1,7 @@
+package com.rmnk.keywords.superKeyWord;
+
+public class WorkingWithSuperKeyWord {
+    public static void main(String[] args) {
+        // here we`ll see how super() methods work on practice
+    }
+}
